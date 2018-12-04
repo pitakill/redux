7a6047c0f203a374e2b07d9d7a92c41061d325df
Lepoldo Caballero <git@pitakill.net>
2018-12-03 18:27:08 -0600

    Split reducer into several functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { createStore } = require('redux')
+const { createStore, combineReducers } = require('redux')
 
 // Reducer | Change are may by pure functions
 const counter = (state = 0, action) => {
@@ -12,8 +12,19 @@ const counter = (state = 0, action) => {
   }
 }
 
+const message = (state = '', action) => {
+  switch (action.type) {
+    case 'CHANGE_IT':
+      return action.message
+    default:
+      return state
+  }
+}
+
+const reducers = combineReducers({counter, message})
+
 // Single source of truth
-const store = createStore(counter)
+const store = createStore(reducers)
 
 store.subscribe(() => console.log(store.getState()))
 
@@ -22,6 +33,8 @@ store.dispatch({ type: 'INCREMENT' })
 store.dispatch({ type: 'INCREMENT' })
 store.dispatch({ type: 'INCREMENT' })
 store.dispatch({ type: 'INCREMENT' })
+
+store.dispatch({ type: 'CHANGE_IT', message: 'Holaaaaaa!!!!!!' })
 
 store.dispatch({ type: 'DECREMENT' })
 store.dispatch({ type: 'DECREMENT' })
